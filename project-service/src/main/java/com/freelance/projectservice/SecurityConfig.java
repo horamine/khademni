86ca1d644968131b05d5b8a801b3e3b0947a7f87
horamine <aminehor.chatgpt@gmail.com>
2026-04-15 20:47:59 +0200

    Update SecurityConfig.java with security configuration

--- project-service/src/main/java/com/freelance/projectservice/SecurityConfig.java
+++ project-service/src/main/java/com/freelance/projectservice/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.freelance.projectservice.security.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,9 +24,9 @@ public class SecurityConfig {
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("GET", "/api/projects").permitAll()
-                        .requestMatchers("GET", "/api/projects/open").permitAll()
-                        .requestMatchers("GET", "/api/projects/{id}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/projects").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/projects/open").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/projects/{id}").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
